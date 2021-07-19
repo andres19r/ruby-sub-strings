493358924dc7627dff726f3c284a239c38be702c
Andres Rivero <andres19rivero@gmail.com>
2021-07-19 16:33:12 -0400

    Finish the project

--- substrings.rb
+++ substrings.rb
@@ -1,14 +1,18 @@
-#!/usr/bin/env ruby
 
-def substrings(word, dictionary)
-  result = Hash.new(0)
-  dictionary.each do |value|
-    if word.include?(value)
-      result[value] += 1
+def substrings(string, dictionary)
+  array_string = string.split.map!{ |el| el.downcase }
+  dictionary.reduce(Hash.new(0)) do |acc,value|
+    array_string.each do |word|
+      if word.include?(value)
+      acc[value] += 1
+      end
     end
+    acc
   end
-  result
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 p substrings("below", dictionary)
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
+# => { "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 }
+# => 11
